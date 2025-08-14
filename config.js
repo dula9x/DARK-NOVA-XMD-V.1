029c65a77343d9f24d3cb59b1df9e885659a5b7a
PRIME-ALPHA-X-DULA-MD <dulinanethmira626@gmail.com>
2025-08-14 17:29:16 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "your sesson id",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/dula9x/PRIME-ALPHA-X-DULA-MD/blob/main/images/PRIME%20ALPHA%20X%20DULA%20MD.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*I'am Alive Now!! PRIME-ALPHA-X-DULA-MD 🤭💗 ආහ් පැටියෝ කොහොමද ?🌝*",
+BOT_OWNER: '94752978237/94770349867', 
+
+
+
+};
