ff998e2d711590fef93f0c605a6a94987150ece4
PRIME-ALPHA-X-DULA-MD <dulinanethmira626@gmail.com>
2025-08-14 18:04:29 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -67,7 +67,7 @@ Your fb video
         from,
         {
           image: {
-            url: "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/fbdownloader.png?raw=true",
+            url: "https://github.com/dula9x/DARK-NOVA-XMD-V.1/blob/main/images/%E1%B4%85%E1%B4%80%CA%80%E1%B4%8B%20%C9%B4%E1%B4%8F%E1%B4%A0%E1%B4%80%20x%E1%B4%8D%E1%B4%85.png?raw=true",
           },
           caption: desc,
         },
